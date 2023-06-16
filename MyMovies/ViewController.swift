4e9deae3ababac2dc925047258f7ab5f6c6dc7cc
Karim Amsha <karim.amsha@gmail.com>
2023-06-16 17:17:48 +0300

    Initial commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Karim Amsha on 16.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
